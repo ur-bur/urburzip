37ed778a2a20fef5c9a965464210a049a06e0442
owunr <sjmskm@gmail.com>
2024-02-08 23:12:30 +0900

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,13 @@ export const metadata: Metadata = {
   누구나 쉽게 배울수 있는 동작과 중독성있는 노래로 부담없이 즐길수 있습니다.
   동기,선후배와 가장 가까워질수 있는 기회이니 놓치지말고 즐거운 추억 같이 만들어봐요! 
   어-리버리 화이팅!!`,
+  icons: {
+    icon: "/favicon.ico",
+  },
   openGraph: {
     title: "어리버리",
-    description: "우주최강 율동패 어리버리"
-  }
+    description: "우주최강 율동패 어리버리",
+  },
 };
 
 export default function RootLayout({
@@ -30,7 +33,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={[inter.className, "flex min-h-screen flex-col items-center justify-center p-1 md:px-24 md:py-10"].join()}>
+      <body
+        className={[
+          inter.className,
+          "flex min-h-screen flex-col items-center justify-center p-1 md:px-24 md:py-10",
+        ].join()}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
