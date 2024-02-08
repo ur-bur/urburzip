52c6c2f63b63d14ebd9507d7b6e9677bbf54063f
owunr <sjmskm@gmail.com>
2024-02-08 23:10:02 +0900

    open graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,10 @@ export const metadata: Metadata = {
   누구나 쉽게 배울수 있는 동작과 중독성있는 노래로 부담없이 즐길수 있습니다.
   동기,선후배와 가장 가까워질수 있는 기회이니 놓치지말고 즐거운 추억 같이 만들어봐요! 
   어-리버리 화이팅!!`,
+  openGraph: {
+    title: "어리버리",
+    description: "우주최강 율동패 어리버리"
+  }
 };
 
 export default function RootLayout({
