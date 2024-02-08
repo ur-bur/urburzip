a9e8621e9f97be4dfce2415c73cf6f245a472f7e
owunr <sjmskm@gmail.com>
2024-02-09 02:11:11 +0900

    add metadata
    keywords, verification, opengraph...

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/Header";
 import { Toaster } from "sonner";
 
-const inter = Inter({ subsets: ["latin"] });
+const notoSansKr = Noto_Sans_KR({
+  weight: ["500"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://urbur.icu"),
   title: {
     template: "%s | URBUR",
     default: "URBUR",
@@ -17,12 +21,28 @@ export const metadata: Metadata = {
   누구나 쉽게 배울수 있는 동작과 중독성있는 노래로 부담없이 즐길수 있습니다.
   동기,선후배와 가장 가까워질수 있는 기회이니 놓치지말고 즐거운 추억 같이 만들어봐요! 
   어-리버리 화이팅!!`,
+  keywords: [
+    "HYU",
+    "CSE",
+    "URBUR",
+    "어리버리",
+    "한양대학교",
+    "컴퓨터소프트웨어학부",
+    "율동패",
+  ],
+  verification: {
+    google: "google",
+    other: {
+      "naver-site-verification": "naver",
+    },
+  },
   icons: {
     icon: "/favicon.ico",
   },
   openGraph: {
     title: "어리버리",
     description: "우주최강 율동패 어리버리",
+    url: "https://urbur.icu",
   },
 };
 
@@ -35,7 +55,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={[
-          inter.className,
+          notoSansKr.className,
           "flex min-h-screen flex-col items-center justify-center p-1 md:px-24 md:py-10",
         ].join()}
       >
