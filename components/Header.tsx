f7d006d12907c2d7599dd570756d8ea64a241a35
angjun04 <peteryoung04@naver.com>
2024-03-05 13:04:19 +0900

    manager인 경우 manage header에 표시

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,8 @@ import { MobileMenu } from "./menu/MobileMenu";
 
 export default async function Header() {
   const supabase = supabaseServer();
-  const { data } = await supabase.auth.getSession();
+  const {data} = await supabase.auth.getSession();
+  const manager = await supabase.from("managers").select("*").eq("id", data.session?.user.id as string);
   return (
     <div className="w-full flex justify-between p-4 md:px-24 md:py-6">
       <Link href={"/"}>
@@ -15,6 +16,7 @@ export default async function Header() {
         <div className="hidden md:flex gap-4 justify-center items-center">
           <div className="flex items-center gap-4">
             {/* <Link href={"/about"}>About</Link> */}
+            {manager.data?.length !== undefined? <Link href={"/manage"}>Manage</Link>:null}
             <Link href={"/apply"}>Apply</Link>
             <Link href={"/manager"}>Manager</Link>
             <Link href={"/music"}>Music</Link>
