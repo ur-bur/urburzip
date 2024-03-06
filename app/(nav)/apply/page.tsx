54b8719a3e86ed26ff14eb633f661af33430b854
angjun04 <peteryoung04@naver.com>
2024-03-06 10:42:51 +0900

    지원 막기

--- app/(nav)/apply/page.tsx
+++ app/(nav)/apply/page.tsx
@@ -8,7 +8,8 @@ export default async function ApplyPage() {
   // await new Promise((resolve) => setTimeout(resolve, 500000));
   return (
     <div className="flex-1 flex items-center justify-center">
-      <ApplyForm />
+      {/* <ApplyForm /> */}
+      <h1>현재는 지원기간이 아닙니다. 2학기 모집을 기대해주세요!</h1>
     </div>
   );
 }
