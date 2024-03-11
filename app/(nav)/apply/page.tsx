5ad29e2988f775d4d251a82af066a6a9525573cc
angjun04 <peteryoung04@naver.com>
2024-03-11 14:15:11 +0900

    open apply

--- app/(nav)/apply/page.tsx
+++ app/(nav)/apply/page.tsx
@@ -7,11 +7,11 @@ export const metadata = {
 export default async function ApplyPage() {
   // await new Promise((resolve) => setTimeout(resolve, 500000));
   return (
-    // <div className="flex-1 flex items-center justify-center">
-    //   <ApplyForm />
-    // </div>
-    <div>
-      현재 어리버리 모집 기간이 아닙니다. 다음 모집을 기대해주세요!!
+    <div className="flex-1 flex items-center justify-center">
+      <ApplyForm />
     </div>
+    // <div>
+    //   현재 어리버리 모집 기간이 아닙니다. 다음 모집을 기대해주세요!!
+    // </div>
   );
 }
